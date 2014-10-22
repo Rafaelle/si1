369df90a236651b90cc2bb722321c788e93eae57
rafaelle <rafaelle.xavier@gmail.com>
2014-10-22 15:30:28 -0300

    dinamica/js/main.js modificado

--- dinamica/js/main.js
+++ dinamica/js/main.js
@@ -1,60 +1,46 @@
 $(function () {
 	$("#adicionar").click( function() {
-		var tema = $(".tema").val();
-		if(tema!=="" ) {
-			var amigo = "<div class=\"panel panel-primary\"><div class=\"panel-heading\"><h3 class=\"panel-title\"><nome>";
-  			amigo += nome;
-  			amigo += "</nome></h3></div><div class=\"panel-body\">";
-      		amigo += "<idade>" + idade + "</idade> anos - <ocupacao>" + ocupacao + "</ocupacao><br><button id = \"editar\">Editar</button> <button id = \"remover\"> Remover </button>";
-   			amigo += "</div></div>";
-
-			$("#lista_pessoas").append(amigo);
-			$("li").addClass("list-group-item");
-		} else {
-			alert("Campos invalidos");
-		}
-		
+		addAnother();		
 	});
+
 	$("#limpar").click( function () {
-        	$("#tema").val("");
-     	});
+        	$(".tema").val("");
+    });
 
 	$("#lista_pessoas").on("click", "#editar", function () {
 		var li = $(this).parent();
 		var nome = li.parent().find("nome");
-		var novoNome = prompt("Nome da pessoa?",nome.html());
-		if(novoNome !== null && novoNome !== ""){
-			nome.html(novoNome);
-		} else if (novoNome === "") {
-			alert("Nome invalido");
-		}
-		var idade = li.children("idade");
-		var novaIdade = prompt("Idade da pessoa?",idade.html());
-		if(isNaN(novaIdade)) {
-			alert("Não é um número");
-		} else {
-			if((novaIdade !== null) && (novaIdade !== "")){
-				idade.html(novaIdade);
-			} else if ((novaIdade === "")) {
-				alert("Idade invalida");
-			}
-		}
-		var ocupacao = li.children("ocupacao");
-		var novaOcupacao = prompt("Ocupação da pessoa?",ocupacao.html());
-		if(novaOcupacao !== null && novaOcupacao !== ""){
-			ocupacao.html(novaOcupacao);
-		} else if (novaOcupacao === "") {
-			alert("Ocupação invalida");
-		}
-
-		
+	
 		
 	});
 	$("#lista_pessoas").on("click", "#remover", function () {
-		$(this).parent().parent().remove();
-		
+		$(this).parent().parent().remove();		
 		
 	});
+
+
+	addAnother = function() {
+		var tema = $(".tema").val();
+		if(tema!=="" ) {
+			var novoTema = "<div class=\"list-group-item\"><tema>";
+  			novoTema += tema;
+  			novoTema += "</tema><button id = \"editar\">Editar</button> <button id = \"remover\"> Remover </button></div>";
+
+	   		var ul = document.getElementById("list");
+		    var li = document.createElement("li");
+		    var children = ul.children.length + 1
+		    li.setAttribute("id", "element"+children)
+		    li.appendChild(document.createTextNode(novoTema));
+		    ul.appendChild(li);
+
+			//$("#lista_aprender").append(novoTema);
+			//$("li").addClass("list-group-item");
+		} else {
+			alert("Campo vazio");
+		}
+
+
+	}
 });
 
 	
