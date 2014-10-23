e095a3bbe24dc368b842fa69ff1caaac4c65c508
Rafaelle <rafaelle.xavier@gmail.com>
2014-10-23 12:35:40 -0300

    ajuste no main.js

--- dinamica/js/main.js
+++ dinamica/js/main.js
@@ -1,49 +1,17 @@
-$(function () {
-	$("#adicionar").click( function() {
-		addAnother();		
-	});
-
+$(function (){
 	$("#limpar").click( function () {
-        	$(".tema").val("");
-    });
-
-	$("#lista_pessoas").on("click", "#editar", function () {
-		var li = $(this).parent();
-		var nome = li.parent().find("nome");
-	
-		
-	});
-	$("#lista_pessoas").on("click", "#remover", function () {
-		$(this).parent().parent().remove();		
-		
+		$("#tema").val("");		
 	});
 
-
-	addAnother = function() {
-		var tema = $(".tema").val();
+	$("#adicionar").click( function(){
+		var tema = $("#tema").val();
 		if(tema!=="" ) {
-			var novoTema = "<div class=\"list-group-item\"><tema>";
-  			novoTema += tema;
-  			novoTema += "</tema><button id = \"editar\">Editar</button> <button id = \"remover\"> Remover </button></div>";
-
-	   		var ul = document.getElementById("list");
-		    var li = document.createElement("li");
-		    var children = ul.children.length + 1
-		    li.setAttribute("id", "element"+children)
-		    li.appendChild(document.createTextNode(novoTema));
-		    ul.appendChild(li);
-
-			//$("#lista_aprender").append(novoTema);
+			//$("#lista_aprender").append(tema);
 			//$("li").addClass("list-group-item");
 		} else {
 			alert("Campo vazio");
 		}
 
-
-	}
+	});
 });
 
-	
-
-
-	
