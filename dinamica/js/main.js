b629821135e3b25a3e6a1ca4c512207e4da2d434
rafaelle <rafaelle.xavier@gmail.com>
2014-10-24 15:55:17 -0300

    inicio função 'aprendido'

--- dinamica/js/main.js
+++ dinamica/js/main.js
@@ -4,21 +4,30 @@ $(function (){
 		var tema = $("#tema").val();
 
 		if(tema!=="") {
-  			var novoTema = "<li class='list-group-item'>" + tema;
-  			novoTema += "<button id = \"aprendido\">Aprendido</button> <button id = \"remover\"> Remover </button></li>";   			
+			var novoTema = "<li id='tema'>" + tema;
+  			novoTema += "<button id = \"aprendido\">Aprendido</button> <button id = \"remover\"> Remover </button></li>";		
 			$("#lista_aprender").append(novoTema);
+			$("li").addClass("list-group-item");
 		} else {
-			alert("Campo vazio");
-		}		
+			alert("Campos invalidos");
+		}	
 	});
 		
 	$("#limpar").click( function () {
 		$("#tema").val("");		
 	});
 
+		
+	$("#lista_aprender").on("click", "#aprendido", function () {
+		var li = $(this).parent();
+		li.remove();
+		$("#lista_aprendido").append(li);		
+		});
+		
 
-
-
+	$("#lista_aprender").on("click", "#remover", function () {
+		$(this).parent().remove();		
+	});
 
 });
 
