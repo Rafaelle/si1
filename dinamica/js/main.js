75502ffa4d1a736319c0b9e981e5ab731878676b
rafaelle <rafaelle.xavier@gmail.com>
2014-10-24 14:29:36 -0300

    adicionando elemento na 'lista a aprender'

--- dinamica/js/main.js
+++ dinamica/js/main.js
@@ -8,17 +8,11 @@ $(function (){
 		var tema = $("#tema").val();
 
 		if(tema!=="") {
-			var novoTema = tema;
-  			novoTema += "<button id = \"editar\">Editar</button> <button id = \"remover\"> Remover </button>";
 
-  			//var novoTema = "<div>" + tema;
-  			//novoTema += "</nome></h3></div><div class=\"panel-body\">";
-      		//novoTema += "button id = \"editar\">Editar</button> <button id = \"remover\"> Remover </button>";
-   			//amigo += "</div></div>"; 
-   			
+  			var novoTema = "<li class='list-group-item'>" + tema;
+  			novoTema += "<button id = \"aprendido\">Aprendido</button> <button id = \"remover\"> Remover </button></li>";   			
 			$("#lista_aprender").append(novoTema);
-			$("li").addClass("list-group-item");
-			$("#editar")
+
 		} else {
 			alert("Campo vazio");
 		}
