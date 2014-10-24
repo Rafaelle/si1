d8177650ce93feb25e4bf35b5466e2f333d64158
rafaelle <rafaelle.xavier@gmail.com>
2014-10-24 15:06:55 -0300

    ajustes no main.js e matriculaRealizada.html

--- dinamica/js/main.js
+++ dinamica/js/main.js
@@ -4,17 +4,14 @@ $(function (){
 		var tema = $("#tema").val();
 
 		if(tema!=="") {
-
   			var novoTema = "<li class='list-group-item'>" + tema;
   			novoTema += "<button id = \"aprendido\">Aprendido</button> <button id = \"remover\"> Remover </button></li>";   			
 			$("#lista_aprender").append(novoTema);
-
 		} else {
 			alert("Campo vazio");
-		}
-		
+		}		
 	});
-	
+		
 	$("#limpar").click( function () {
 		$("#tema").val("");		
 	});
