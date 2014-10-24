03186b9a4f7f39fd75800f61e0848c23c35698df
rafaelle <rafaelle.xavier@gmail.com>
2014-10-24 08:36:47 -0300

    adicionando novo item

--- dinamica/js/main.js
+++ dinamica/js/main.js
@@ -3,15 +3,32 @@ $(function (){
 		$("#tema").val("");		
 	});
 
-	$("#adicionar").click( function(){
+
+	$("#adicionar").click( function() {
 		var tema = $("#tema").val();
-		if(tema!=="" ) {
-			//$("#lista_aprender").append(tema);
-			//$("li").addClass("list-group-item");
+
+		if(tema!=="") {
+			var novoTema = tema;
+  			novoTema += "<button id = \"editar\">Editar</button> <button id = \"remover\"> Remover </button>";
+
+  			//var novoTema = "<div>" + tema;
+  			//novoTema += "</nome></h3></div><div class=\"panel-body\">";
+      		//novoTema += "button id = \"editar\">Editar</button> <button id = \"remover\"> Remover </button>";
+   			//amigo += "</div></div>"; 
+   			
+			$("#lista_aprender").append(novoTema);
+			$("li").addClass("list-group-item");
+			$("#editar")
 		} else {
 			alert("Campo vazio");
 		}
-
+		
 	});
+
+
+
+
+
+
 });
 
