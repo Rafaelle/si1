f5685380b0a0ac41ebb306a6328eac904beee7a0
Rafaelle <rafaelle.xavier@gmail.com>
2014-10-27 22:35:55 -0300

    versão final

--- dinamica/js/main.js
+++ dinamica/js/main.js
@@ -1,12 +1,15 @@
 $(function (){
 	$( "#addConteudo" ).submit(function(e) {
 		e.preventDefault();
-		var conteudo = "<conteudo> " + $("#conteudo").val() + "</conteudo>";
+		var conteudo = "<tema> " + $("#conteudo").val() + "</tema>";
 		var buttonAprendido = "<button class = \"aprendido\">Aprendido</button>";
 		var buttonRemover = "<button class = \"remover\"> Remover </button>";
 		var novoConteudo = "<li id='conteudo'>" + conteudo + buttonAprendido + buttonRemover +"</li>";		
 		$("#lista_aprender").append(novoConteudo);
 		$("li").addClass("list-group-item");
+		$(".aprendido").addClass("btn btn-success");
+		$(".remover").addClass("btn btn-danger");
+
 		limparInput();
 
 	});
@@ -17,7 +20,7 @@ $(function (){
 
 	$("#lista_aprender").on("click", ".aprendido", function () {
 		var li = $(this).parent();
-		var conteudoAprendido = li.find("conteudo").text();
+		var conteudoAprendido = li.find("tema").text();
 		$("#lista_aprendido").append("<li>" + conteudoAprendido + "</li>");
 		$("li").addClass("list-group-item");
 		li.remove();
